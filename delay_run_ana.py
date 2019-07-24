2230df9f001ec5c53277c12430743ee8be2c2280
sgaobnl <gao33.bnl@gmail.com>
2019-07-24 09:40:27 -0400

    Update delay_run_ana.py

--- delay_run_ana.py
+++ delay_run_ana.py
@@ -10,6 +10,7 @@ import os
 from sys import exit
 import os.path
 import math
+import time
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
@@ -19,13 +20,17 @@ import matplotlib.mlab as mlab
 
 import pickle
 fn = "D:\ColdADC\Data_chn0_dly1.bin"
-open (fn, 'rb') as fp:
+with open (fn, 'rb') as fp:
     chns = pickle.load(fp)
-    fig = plt.figure(figsize=(16,9))
-    n = 200
-    plt.plot(np.arange(n), chns[0][0:n])
-    plt.scatter(np.arange(n), chns[0][0:n])
+    
+fig = plt.figure(figsize=(16,9))
+n = 400
+plt.plot(np.arange(n), chns[0][0:n])
+plt.scatter(np.arange(n), chns[0][0:n])
 
-    plt.show()
-    plt.close()
+#plt.savefig("d:\abc.png")
+plt.show()
+
+print ("xxxx")
+#plt.close()
 
