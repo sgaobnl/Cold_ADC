ccdebe712d33dccdd12aaf8fe8d5f8a4c58a3323
Shanshan Gao <gao33.bnl@gmail.com>
2019-07-24 00:09:29 -0400

    Update delay_run_ana.py

--- delay_run_ana.py
+++ delay_run_ana.py
@@ -5,6 +5,27 @@ Created on Wed Jul 24 00:00:54 2019
 @author: protoDUNE
 """
 
-import pickle
+import numpy as np
+import os
+from sys import exit
+import os.path
+import math
 
-D:\ColdADC\Data_chn0_dly1.bin
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
+import matplotlib.mlab as mlab
+
+
+import pickle
+fn = "D:\ColdADC\Data_chn0_dly1.bin"
+open (fn, 'rb') as fp:
+    chns = pickle.load(fp)
+    fig = plt.figure(figsize=(16,9))
+    n = 200
+    plt.plot(np.arange(n), chns[0][0:n])
+    plt.scatter(np.arange(n), chns[0][0:n])
+
+    plt.show()
+    plt.close()
+
