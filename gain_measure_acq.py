897ecf14962d80228ebc83aed4db73ac1a79ac0d
Shanshan Gao <gao33.bnl@gmail.com>
2019-08-19 12:27:17 -0400

    Update gain_measure_acq.py

--- gain_measure_acq.py
+++ gain_measure_acq.py
@@ -104,9 +104,9 @@ for asic_dac in range(3,0x10,1):
         for i in range(0,avg_n):
             for j in [1]:
                 if i == 0:
-                    avg_chns = np.array(chns[j][poft+200*i:poft+200+200*i]&0xffff)
+                    avg_chns = np.array(chns[j][poft+200*i:poft+200+200*i])&0xffff)
                 else:
-                    avg_chns = avg_chns + np.array(chns[j][poft+200*i:poft+200+200*i]&0xffff)
+                    avg_chns = avg_chns + (np.array(chns[j][poft+200*i:poft+200+200*i])&0xffff)
     
         avg_chns = avg_chns//avg_n
         for i in range(len(avg_chns)):
